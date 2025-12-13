ee8333d38335d254255639987820c178d8505591
Rahul Pandita <rpandita1999@outlook.com>
2025-12-13 02:50:30 +0000

    write method to retrieve k number of context texts from vector data

--- lib/load_json.py
+++ lib/load_json.py
@@ -8,8 +8,8 @@ class InputExtraction:
     sample_chat_jsons = sorted(SAMPLE_DATA_DIR_PATH.glob("sample-chat*.json"))
     sample_vector_jsons = sorted(SAMPLE_DATA_DIR_PATH.glob("sample_context*.json"))
     
-    # k = number of retrieved contexts to get from sample vector data
-    def __init__(self, conversation_number: int, k:int = 5, ):
+   
+    def __init__(self, conversation_number: int):
         conversation_path = type(self).sample_chat_jsons[conversation_number]
         context_path = type(self).sample_vector_jsons[conversation_number]
         with open(conversation_path, "r") as f:
@@ -24,9 +24,14 @@ class InputExtraction:
         for turn in self.conversation_turns.reverse():
             if turn["role"] == "User":
                 return turn["message"]
-            
-    # def _extract_context_texts(self):
-    #     for data in self.vector_data:
+    
+     # k = number of retrieved contexts to get from sample vector data
+    def _extract_context_texts(self, k:int = 5):
+        texts = []
+        for data in self.vector_data:
+            text = data["text"]
+            texts.append(text)
+        return texts[:k]
 
 
         
